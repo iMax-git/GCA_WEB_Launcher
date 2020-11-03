7f52c8deb2031357923f26530069caa50e09d6d0
iMax <45369969+iMax-git@users.noreply.github.com>
2020-11-03 19:03:34 +0100

    Cleaning include.db.php

--- includes/include.db.php
+++ includes/include.db.php
@@ -1,12 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "OMYRSg2qyEuuYXpQ";
-$dbname = "gotham";
+$password = "";
+$dbname = "";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
@@ -18,25 +16,13 @@ function whitelisted($game = "None",$steamid = "None"){
   global $result, $lists;
   assert($game != "None");
   assert($steamid != "None");
-  /*
-  echo "________________<br>";
-  echo "game = ",$game,"<br>";
-  echo "steamid = ",$steamid,"<br>";
-  echo "________________<br>";
-  */
   if($game == "fivem"){
-    //echo "FIVEM <br>";
     while($row = $result->fetch_assoc()){
-      //echo "identifier: " . $row["identifier"]. " <br>";
       if($row["identifier"] == $steamid){
-        //echo "identifier ended: " . $row["identifier"] . " <br>";
         return "True";
       }
     }
   }
 }
-
-
-//echo whitelisted("fivem","steam:11000010572fcd6");
 $conn->close();
 ?>
