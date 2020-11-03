55245eab8a949daabea8351ba78cda1b05e8c3b2
iMax <45369969+iMax-git@users.noreply.github.com>
2020-11-03 19:04:42 +0100

    Cleaning SteamConfig.php

--- includes/SteamConfig.php
+++ includes/SteamConfig.php
@@ -1,7 +1,7 @@
 <?php
 //Version 4.0
-$steamauth['apikey'] = "70518F7B84BEC85CB8FC85B0FC5D10C5"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = "146.59.151.43/launcher/"; // The main URL of your website displayed in the login page
+$steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = ""; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
